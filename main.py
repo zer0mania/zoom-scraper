cd633e970b6279f5de8df07be006083587eb8b9a
zer0mania <48740591+zer0mania@users.noreply.github.com>
2022-06-07 09:20:35 -0700

    Update main.py

--- main.py
+++ main.py
@@ -3,33 +3,21 @@ import random
 import threading
 from time import sleep
 import ctypes
-from urllib3.exceptions import InsecureRequestWarning
-import ssl
+import sys
+import json
 import os
 
 failed = 0
 found = 0
 
+with open('config.json') as f:
+    data = json.load(f)
+
+automatic = data['automatic']
+proxyType = data['proxyType']
+filename = data['filename']
+
 global_lock = threading.Lock()
-
-ctypes.windll.kernel32.SetConsoleTitleW(f"Threads: {str(threading.active_count()-1)} Failed: {str(failed)} Found: {str(found)}")
-
-requests.packages.urllib3.disable_warnings(category=InsecureRequestWarning)
-ssl._create_default_https_context = ssl._create_unverified_context
-
-y = int(input("Threads: "))
-proxyChoose = True
-while proxyChoose:
-    proxyType = input("Select proxy type:\n[0] - http\n[1] - socks4\n[2] - socks5 > ")
-    if proxyType == "0":
-        proxyType = "http"
-        proxyChoose = False
-    elif proxyType == "1":
-        proxyType = "socks4"
-        proxyChoose = False
-    elif proxyType == "2":
-        proxyType = "socks5"
-        proxyChoose = False
 
 def check_id(id):
     proxy = {f'https': f'{proxyType}://{random.choice(proxyList)}'}
@@ -42,7 +30,7 @@ def check_id(id):
     data = f"--------------------------e4989a6b36dcd724\r\nContent-Disposition: form-data; name=\"clientUserSameAsWebUser\"\r\n\r\n1\r\n--------------------------e4989a6b36dcd724\r\nContent-Disposition: form-data; name=\"cv\"\r\n\r\n5.10.7.6120\r\n--------------------------e4989a6b36dcd724\r\nContent-Disposition: form-data; name=\"deviceId\"\r\n\r\n{id}\r\n--------------------------e4989a6b36dcd724\r\nContent-Disposition: form-data; name=\"email\"\r\n\r\ncoolkid69@gmail.com\r\n--------------------------e4989a6b36dcd724\r\nContent-Disposition: form-data; name=\"mn\"\r\n\r\n{id}\r\n--------------------------e4989a6b36dcd724\r\nContent-Disposition: form-data; name=\"needWebCheckDLP\"\r\n\r\n0\r\n--------------------------e4989a6b36dcd724\r\nContent-Disposition: form-data; name=\"source\"\r\n\r\nclient\r\n--------------------------e4989a6b36dcd724\r\nContent-Disposition: form-data; name=\"uname\"\r\n\r\n{id}\r\n--------------------------e4989a6b36dcd724--\r\n"
     
     try:
-        response = requests.post(url, headers=headers, data=data, proxies=proxy, verify=False)
+        response = requests.post(url, headers=headers, data=data, proxies=proxy)
     except:
         return None
 
@@ -51,6 +39,7 @@ def check_id(id):
         return None
 
     if not "zoom" in response.text:
+        print(response.text)
         return False
     else:
         print(response.text)
@@ -63,6 +52,15 @@ def stats(type):
     elif type == "failed":
         failed = failed + 1
 
+def setTitle(_str):
+    system = os.name
+    if system == 'nt':
+        ctypes.windll.kernel32.SetConsoleTitleW(_str)
+    elif system == 'posix':
+        sys.stdout.write(_str)
+    else:
+        pass
+
 def write_to_file():
     with global_lock:
         with open("ids.html", "a") as file:
@@ -74,15 +72,25 @@ def readFile(filename,method):
         content = [line.strip('\n') for line in f]
         return content
 
-def readProxiesFile():
+def DownloadFile(url):
+    local_filename = f"{proxyType}.txt"
+    r = requests.get(url)
+    f = open(local_filename, 'wb')
+    for chunk in r.iter_content(chunk_size=512 * 1024): 
+        if chunk: # filter out keep-alive new chunks
+            f.write(chunk)
+    f.close()
+    return local_filename
+
+def readProxiesFile(_str):
     restartTry = True
     while restartTry:
         try:
-            proxies = readFile("proxies.txt", 'r')
+            proxies = readFile(_str, 'r')
             restartTry = False
             return proxies
         except:
-            print("Failed to open proxies.txt")
+            print(f"Failed to open {_str}")
             restartTry = True
 
 def mythread():
@@ -96,19 +104,39 @@ def mythread():
         returned = check_id(id)
         if returned:
             stats("found")
-            ctypes.windll.kernel32.SetConsoleTitleW(f"Threads: {str(threading.active_count()-1)} Failed: {str(failed)} Found: {str(found)}")
+            setTitle(f"Threads: {str(threading.active_count()-1)} Failed: {str(failed)} Found: {str(found)}")
             with global_lock:
                 with open("ids.html", "a") as file:
-                    file.write(f'\n<a class="code" href="https://zoom.us/j/{id}">{found}: https://zoom.us/j/{id}</a>')
+                    file.write(f'\n<a class="code" target="_blank" href="https://zoom.us/j/{id}">{found}: https://zoom.us/j/{id}</a>')
                     file.write("<br>")
         else:
             stats("failed")
-            ctypes.windll.kernel32.SetConsoleTitleW(f"Threads: {str(threading.active_count()-1)} Failed: {str(failed)} Found: {str(found)}")
+            setTitle(f"Threads: {str(threading.active_count()-1)} Failed: {str(failed)} Found: {str(found)}")
 
-proxyList = readProxiesFile()
+if not automatic == "true":
+    proxyList = readProxiesFile(filename)
+
+setTitle(f"Threads: {str(threading.active_count()-1)} Failed: {str(failed)} Found: {str(found)}")
+threads = int(input("Threads: "))
+if automatic == "true":
+    proxyChoose = True
+    while proxyChoose:
+        proxyType = input("Select proxy type:\n[0] - http\n[1] - socks4\n[2] - socks5 > ")
+        if proxyType == "0":
+            proxyType = "http"
+            proxyChoose = False
+        elif proxyType == "1":
+            proxyType = "socks4"
+            proxyChoose = False
+        elif proxyType == "2":
+            proxyType = "socks5"
+            proxyChoose = False
+    print("Downloading proxies..")
+    proxyList = readProxiesFile(DownloadFile(f"https://api.proxyscrape.com/v2/?request=getproxies&protocol={proxyType}&timeout=10000&country=all"))
+    print("Proxies downloaded")
 
 def main():
-    for i in range(y):
+    for i in range(threads):
         t = threading.Thread(target=mythread)
         t.start()
         sleep(.01)
