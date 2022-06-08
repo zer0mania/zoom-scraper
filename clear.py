dfca0598777d6acd014edbcdceea4fdeb0824b52
Thomas Aung <92353124+Thomasaung12345@users.noreply.github.com>
2022-06-08 09:41:21 +0630

    Update clear.py

--- clear.py
+++ clear.py
@@ -1,7 +1,7 @@
 with open("ids.html", 'w') as f:
     f.truncate(0)
     f.write('''<title>Saved Zoom Links</title>
-    <meta http-equiv="refresh" content="5">
+    <link rel="icon" href="https://th.bing.com/th/id/R.e3fe62866b3bdbeea5bf196f6c672c9d?rik=fwnXyx2KXlxoEA&pid=ImgRaw&r=0" type="image/icon type"><meta http-equiv="refresh" content="5">
     <link href="style.css" rel="stylesheet">
     <body style="background-color:black;">
     <h2> Zoom Links!</h2>''')
